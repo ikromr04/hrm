b8084b6a00f7f68e3027499e55f86eb310bd753c
ikromr04 <ikromr04@gmail.com>
2026-04-03 17:36:15 +0500

    refactor: group routes by controllers

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,25 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\EmployeeController;
-use App\Http\Controllers\Api\V1\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/images/{path}', [ImageController::class, 'show'])
-        ->where('path', '.*')
-        ->name('images.show');
-
     Route::controller(AuthController::class)->group(function () {
         Route::get('/me', 'me')->name('me');
         Route::delete('/logout', 'logout')->name('logout');
         Route::delete('/tokens', 'logoutAll')->name('logoutAll');
-    });
-
-    Route::controller(EmployeeController::class)->group(function () {
-        Route::get('/employees', 'index')->name('employees');
-        Route::get('/employees/{user}', 'show')->name('employees.show');
     });
 });
