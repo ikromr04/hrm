d0eebef97865649f65fe88c9a893f2c7c24231a2
ikromr04 <ikromr04@gmail.com>
2026-04-07 14:37:46 +0500

    feat(user-details): add CRUD API routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\V1\UserDetailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -20,5 +21,13 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::patch('users/{user}', 'update')->name('users.update');
         Route::delete('users/{user}', 'destroy')->name('users.destroy');
         Route::post('users/{user}/avatar', 'uploadAvatar')->name('users.avatar');
+    });
+
+    Route::controller(UserDetailController::class)->group(function () {
+        Route::get('user-details', 'index')->name('user-details.index');
+        Route::post('user-details', 'store')->name('user-details.store');
+        Route::get('user-details/{user-detail}', 'show')->name('user-details.show');
+        Route::patch('user-details/{user-detail}', 'update')->name('user-details.update');
+        Route::delete('user-details/{user-detail}', 'destroy')->name('user-details.destroy');
     });
 });
